3545d16166e707392353b81ecc2c67f9e385272d
221boy221 <warrock.boy@hotmail.com>
2015-02-08 18:47:52 +0100

    Scrolling background script

---
+++ ProjectMap/Assets/Scripts/Background/Scrolling.cs
@@ -0,0 +1,22 @@
+﻿﻿using UnityEngine;
+using System.Collections;
+
+// Boy
+
+public class Scrolling : MonoBehaviour {
+
+    [SerializeField] 
+    private float speed = 0f;
+    private float position = 0f;
+
+    private void Update() {
+        position += speed * Time.deltaTime;
+
+        if (position > 1.0f) {
+            position = 0f;
+        }
+
+        renderer.material.mainTextureOffset = new Vector2(position, 0);
+    }
+
+}
