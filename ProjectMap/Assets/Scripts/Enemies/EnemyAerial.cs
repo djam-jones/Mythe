90b2dbd62bcbfc3fc6c066e9909ee8818c99c8a2
Djamali Shawn Jones <15546@ma-web.nl>
2015-02-09 12:18:18 +0100

    Enemy Types

---
+++ ProjectMap/Assets/Scripts/Enemies/EnemyAerial.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAerial : EnemyBase {
+
+	void Start()
+	{
+		//Set the type of enemy.
+		typeEnemy = Enemy.aerial;
+		SetHealth();
+	}
+
+	void Update()
+	{
+		//Run the Update function of the base class, before this update functions
+		base.Update();
+	}
+}
---
+++ ProjectMap/Assets/Scripts/Enemies/EnemyBasic.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBasic : EnemyBase {
+
+	void Start()
+	{
+		//Set the type of enemy.
+		typeEnemy = Enemy.basic;
+		SetHealth();
+	}
+
+	void Update()
+	{
+		//Run the Update function of the base class, before this update functions
+		base.Update();
+	}
+
+}
