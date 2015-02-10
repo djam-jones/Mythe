2ffa4639b4aecd8bc9a3965f872da8270d593c06
Boy Voesten <warrock.boy@hotmail.com>
2015-02-10 20:30:24 +0100

    Fixed all merge problems

---
+++ ProjectMap/Assets/Archive/Scripts/Player/Rotation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+    private Vector2 mousePos;
+
+    void FixedUpdate() {
+        UpdateRot();
+    }
+
+    void UpdateRot() {
+        // Position of this object using Pixels
+        Vector2 objPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        mousePos.x = Input.mousePosition.x - objPos.x;
+        mousePos.y = Input.mousePosition.y - objPos.y;
+
+        float rot = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, rot));
+    }
+
+}
---
+++ ProjectMap/Assets/Scripts/UI/Joystick.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Joystick : MonoBehaviour {
+
+    private Vector2 mousePosition;
+    private Vector2 originalPos;
+    public float moveSpeed = 0.1f;
+    private bool move;
+
+    void Start() {
+        originalPos = transform.position;
+    }
+
+    public void Drag(bool _move) {
+        move = _move;
+    }
+
+    public void Update() {
+        if (move) {
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        } else {
+            transform.position = Vector2.Lerp(transform.position, originalPos, moveSpeed * 2);
+        }
+    }
+}
