a29dc5511a3822dde23838cd53a13c62208db90a
Djamali Shawn Jones <15546@ma-web.nl>
2015-02-13 14:00:24 +0100

    Button Movement, Fade on Level Change

---
+++ ProjectMap/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	void Update()
+	{
+		float x = Input.GetAxis("Horizontal") * 10 * Time.deltaTime;
+		float y = Input.GetAxis("Vertical") * 10 * Time.deltaTime;
+
+		transform.Translate(new Vector2(x, y));
+	}
+}
