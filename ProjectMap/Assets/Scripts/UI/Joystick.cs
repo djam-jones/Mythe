006ab99b88c91a37b71c8f327ee1f253e5c061b2
Boy Voesten <warrock.boy@hotmail.com>
2015-02-13 10:26:46 +0100

    Joystick improvement, canvas ui is now a prefab

--- ProjectMap/Assets/Scripts/UI/Joystick.cs
+++ ProjectMap/Assets/Scripts/UI/Joystick.cs
@@ -1,39 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Joystick : MonoBehaviour 
-{
+public class Joystick : MonoBehaviour {
 
     private Vector2 mousePosition;
     private Vector2 originalPos;
-    public float moveSpeed = 0.1f;
+    private float range = 0.3f;
+    private float pivotToObj;
     private bool move;
+    public float moveSpeed = 0.1f;
 
-    public float jXaxis;
-    public float jYaxis;
-    private float range = 0.5f;
-
-    void Start() 
-    {
+    void Start() {
         originalPos = transform.position;
     }
 
-    public void Drag(bool _move) 
-    {
+    public void Drag(bool _move) {
         move = _move;
     }
 
-    public void FixedUpdate() 
-    {
+    public void FixedUpdate() {
         mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        
-        float distance = (mousePosition - originalPos).magnitude;
 
-        if (move) 
-        {
-            if (distance <= range)
-            {
+        float distance = (mousePosition - originalPos).magnitude;
+        float pivotToObj = ((Vector2)transform.position - originalPos).magnitude;
+        float offsetX;
+
+        if (move) {
+            if (distance <= range) {
                 /* Smooth movement */
                 //Vector2 offset = mousePosition - (Vector2)transform.position;
                 //if (offset.magnitude > moveSpeed)
@@ -42,22 +36,29 @@ public class Joystick : MonoBehaviour
                 //offset *= moveSpeed;
                 //}
                 //transform.position += (Vector3)offset;
-                
+
                 transform.position = mousePosition;
-            }
-            else 
-            {
+            } else {
                 Vector2 step = mousePosition - originalPos;
                 step.Normalize();
                 step *= range;
 
                 transform.position = originalPos + step;
             }
-        }
-        else 
-        {
+        } else {
             transform.position = Vector2.Lerp(transform.position, originalPos, moveSpeed * 2);
         }
 
+        // This will prevent the values from going crazy when trying to return to it's original position
+        if (pivotToObj > 0.1f) {
+            pivotToObj = ((Vector2)transform.position - originalPos).magnitude;
+            offsetX = (transform.position.x - originalPos.x) / range;
+        } else {
+            pivotToObj = 0;
+            offsetX = 0;
+        }
+
+        Debug.Log(pivotToObj);
+        Debug.Log(offsetX);
     }
 }
