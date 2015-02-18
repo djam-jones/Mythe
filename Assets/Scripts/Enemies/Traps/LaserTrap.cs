d547dfadd71dab74a8c3c84d46f38487fc09806d
Djamali Shawn Jones <15546@ma-web.nl>
2015-02-18 20:33:38 +0100

    Attempt to push LaserTrap.cs again
    Failed the first time.

---
+++ Assets/Scripts/Enemies/Traps/LaserTrap.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserTrap : MonoBehaviour 
+{
+	private float _laserTimer;
+	public float interval = 2f;
+	private bool _laserOn;
+
+	public GameObject laserBeam;
+
+	void Start()
+	{
+		//Set the default value of the Timer
+		_laserTimer = interval;
+	}
+
+	void FixedUpdate()
+	{
+		_laserTimer -= Time.fixedDeltaTime;
+
+		if(_laserTimer <= 0)
+		{
+			_laserOn = !_laserOn;
+
+			laserBeam.SetActive(_laserOn);
+
+			_laserTimer = interval;
+		}
+	}
+}
