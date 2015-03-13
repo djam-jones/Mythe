c092f08dcedb46915e6754186152067a5c3ab4ec
rob verhoef <16617@ma-web.nl>
2015-03-13 11:34:28 +0100

    Revised MoveThroughTubes

--- Assets/Scripts/Interactables/MoveThroughTube.cs
+++ Assets/Scripts/Interactables/MoveThroughTube.cs
@@ -5,13 +5,23 @@ using System.Collections;
 public class MoveThroughTube : MonoBehaviour 
 {
 	[SerializeField]
-	private Transform _Destination;
+	private Transform _destination;
+	[SerializeField]
+	private GameObject _alien;
 
 	void OnTriggerEnter2D (Collider2D Other)
 	{
 		if (Other.tag == "Alien")
 		{
-			Other.transform.position = _Destination.position;
+			StartCoroutine(GoThrough());
 		}
 	}
+	IEnumerator GoThrough ()
+	{
+		_alien.SetActive (false);
+		yield return new WaitForSeconds(1);
+		_alien.transform.position = _destination.position;
+		_alien.SetActive (true);
+	}
+
 }
