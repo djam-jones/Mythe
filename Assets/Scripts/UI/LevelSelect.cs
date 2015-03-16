966b8dbf3b7fcdd065a18bbefeb090be3f237604
rob verhoef <16617@ma-web.nl>
2015-03-16 20:51:54 +0100

    Added LevelSelect

---
+++ Assets/Scripts/UI/LevelSelect.cs
@@ -0,0 +1,31 @@
+﻿//written by Rob Verhoef
+using UnityEngine;
+using System.Collections;
+
+public class LevelSelect : MonoBehaviour 
+{
+	public void LoadLevel1 ()
+	{
+		Application.LoadLevel ("Level01");
+	}
+
+	public void LoadLevel2 ()
+	{
+		Application.LoadLevel ("Level02");
+	}
+
+	public void LoadLevel3 ()
+	{
+		Application.LoadLevel ("Level03");
+	}
+
+	public void LoadLevel4 ()
+	{
+		Application.LoadLevel ("Level04");
+	}
+
+	public void LoadLevel5 ()
+	{
+		Application.LoadLevel ("Level05");
+	}
+}
