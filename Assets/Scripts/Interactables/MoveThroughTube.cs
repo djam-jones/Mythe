187fa80e97e786f4fc6d344669e7a3c5bb6a6df7
Boy Voesten <warrock.boy@hotmail.com>
2015-03-19 10:07:10 +0100

    Improved the MoveThroughTube, Camera now follows alien while in tube, small lvl edits.

--- Assets/Scripts/Interactables/MoveThroughTube.cs
+++ Assets/Scripts/Interactables/MoveThroughTube.cs
@@ -8,20 +8,31 @@ public class MoveThroughTube : MonoBehaviour
 	private Transform _destination;
 	[SerializeField]
 	private GameObject _alien;
+    private SpriteRenderer _alienSpriteR;
+    private BoxCollider2D _alienColl;
+    private Rigidbody2D _alienRB;
 
 	void OnTriggerEnter2D (Collider2D Other)
 	{
-		if (Other.tag == "Alien")
+		if (Other.tag == _alien.gameObject.tag)
 		{
 			StartCoroutine(GoThrough());
 		}
 	}
 	IEnumerator GoThrough ()
 	{
-		_alien.SetActive (false);
+        _alienSpriteR = _alien.GetComponentInChildren<SpriteRenderer>();
+        _alienColl = _alien.GetComponentInChildren<BoxCollider2D>();
+        _alienRB = _alien.GetComponentInChildren<Rigidbody2D>();
+        _alienSpriteR.enabled = false;
+        _alienColl.enabled = false;
+        _alienRB.isKinematic = true;
+        _alien.transform.position = _destination.position;
 		yield return new WaitForSeconds(1);
-		_alien.transform.position = _destination.position;
-		_alien.SetActive (true);
-	}
+        _alien.transform.position = _destination.position;
+        _alienSpriteR.enabled = true;
+        _alienColl.enabled = true;
+        _alienRB.isKinematic = false;
+    }
 
 }
