e05d6f487f46d0720b8474a20713783ad14d6ca7
rob verhoef <16617@ma-web.nl>
2015-03-23 09:49:21 +0100

    Fixed bug in ObjectManipulation

--- Assets/Scripts/Player/ObjectManipulation.cs
+++ Assets/Scripts/Player/ObjectManipulation.cs
@@ -6,46 +6,68 @@ using System.Collections;
 public class ObjectManipulation : MonoBehaviour 
 {
 	private Vector3 _mousePos;
-	[SerializeField]
-	private Camera _camera;
-	[SerializeField]
-	private Scrollbar _pointBar;
-	[SerializeField]
-	private float _decrease;
-	[SerializeField]
-	private float _increase;
-	[SerializeField]
-	private float _points;
+	[SerializeField]private Camera _camera;
+	[SerializeField]private Scrollbar _pointBar;
+	[SerializeField]private float _decrease;
+	[SerializeField]private float _increase;
+	[SerializeField]private float _points;
 	private bool _recharge;
+	[SerializeField]private Transform _dragObject;
 
 	void Update () 
 	{
 
-		_mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);	
+		_mousePos = _camera.ScreenToWorldPoint (Input.mousePosition);	
 		_mousePos.z = 0;
 
-		if(Input.GetMouseButton (0) && _points > 0)
+		if (Input.GetMouseButtonDown (0) && _points > 0) 
 		{
-			RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+			RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero);
 			{
-				if(hit.collider.tag == AllTagsConstants.objectTag && _mousePos.x - hit.collider.transform.position.x <= 1.5 && _mousePos.y - hit.collider.transform.position.y <= 1.5)
+				if (hit.collider.tag == AllTagsConstants.objectTag && _mousePos.x - hit.collider.transform.position.x <= 1.5 && _mousePos.y - hit.collider.transform.position.y <= 1.5) 
 				{
-					hit.collider.transform.position = _mousePos;
+					_dragObject = hit.collider.transform;
 					_recharge = false;
-					_points -= Time.deltaTime * _decrease;
-					_pointBar.size = _points/100;
 				}
 			}
-		}
-
-		else if(Input.GetMouseButtonUp (0))
+		} 
+		else if (Input.GetMouseButtonUp (0)) 
 		{
 			_recharge = true;
+			_dragObject = null;
 		}
-		if(_recharge == true && _points < 100)
+		else if (_points <= 0)
+		{
+			_dragObject = null;
+		}
+
+		if (!_recharge && _dragObject != null) 
+		{
+			_dragObject.position = _mousePos;
+			_points -= Time.deltaTime * _decrease;
+			_pointBar.size = _points / 100;
+		}
+		else if(_recharge && _points < 100)
 		{
 			_points += Time.deltaTime * _increase;
-			_pointBar.size = _points/100f;
+			_pointBar.size = _points / 100;
 		}
+
+	}
+
+	private void Drag ()
+	{
+
+		/*if (!_recharge && _dragObject != null) 
+		{
+			_dragObject.position = _mousePos;
+			_points -= Time.deltaTime * _decrease;
+			_pointBar.size = _points / 100;
+		}
+		else if(_recharge && _points < 100)
+		{
+			_points += Time.deltaTime * _increase;
+			_pointBar.size = _points / 100;
+		}*/
 	}
 }
