c0c38aa6fe32912d84a7d705a1aa141e6eab117d
rob verhoef <16617@ma-web.nl>
2015-03-23 11:40:37 +0100

    Added MainMenu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour 
+{
+	public void Play ()
+	{
+		Application.LoadLevel ("LevelSelect");
+	}
+
+	public void Credits ()
+	{
+		Application.LoadLevel ("Credits");
+	}
+
+	public void Quit ()
+	{
+		Application.Quit ();
+	}
+}
