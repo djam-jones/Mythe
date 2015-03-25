19e4ba53ebc20c23b268cdd2d90c343a7d0db331
rob verhoef <16617@ma-web.nl>
2015-03-25 08:57:04 +0100

    Started on Respawn, added Credits, fixed MainMenu & LevelSelect

---
+++ Assets/Scripts/Player/PlayerRespawn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRespawn : MonoBehaviour 
+{
+	[SerializeField]private GameObject _respawnPoint;
+
+	private void OnCollisionEnter2D(Collision2D Col)
+	{
+		if(Col.transform.tag == AllTagsConstants.trapTag)
+		{
+			transform.position = _respawnPoint.transform.position;
+		}
+	}
+
+	private void OnTriggerEnter2d(Collider2D Col)
+	{
+		if(Col.transform.tag == AllTagsConstants.respawnTag)
+		{
+			_respawnPoint.transform.position = Col.transform.position;
+		}
+	}
+}
